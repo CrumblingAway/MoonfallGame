114874168915ab123b3a25e78dd5918e2395112c
CrumblingAway <antonkid@gmail.com>
2024-03-06 15:12:07 +0200

    Minigame: Add basic game.

--- scripts/tree_minigame.gd
+++ scripts/tree_minigame.gd
@@ -2,11 +2,27 @@ class_name TreeMinigame extends Node
 
 signal end_minigame
 
-@export var _camera : Camera2D
+@export var _player_bar : RigidBody2D
+@export var _target_bar : Area2D
+
+var _score : int = 0
+var _are_targets_overlapping : bool
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("action"):
-		emit_signal("end_minigame")
+		if _are_targets_overlapping:
+			_score += 1
+			print("Score: " + str(_score))
+	
+	if _player_bar.position.y > 300.0:
+		print("Score: " + str(_score))
+		end_minigame.emit()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	_are_targets_overlapping = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	_are_targets_overlapping = false
