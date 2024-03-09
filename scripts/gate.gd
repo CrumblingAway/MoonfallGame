c3c0c77b9bb272a411e7adabdc0f96c96132a997
CrumblingAway <antonkid@gmail.com>
2024-03-09 18:28:37 +0200

    Win: Add win label.

--- scripts/gate.gd
+++ scripts/gate.gd
@@ -3,7 +3,7 @@ class_name Gate extends Node2D
 @export var _num_of_keys_required : int = 3
 @export var _is_player_gate : bool = false
 
-@onready var _win_area : Area2D = $Area2D
+@onready var _win_area : Area2D = $WinArea
 
 func _ready() -> void:
 	remove_child(_win_area)
@@ -17,7 +17,3 @@ func open() -> void:
 
 func get_num_of_keys_required() -> int:
 	return _num_of_keys_required
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		print("You won!")
--- scripts/ui_script.gd
+++ scripts/ui_script.gd
@@ -11,3 +11,13 @@ func _process(delta: float) -> void:
 
 func _on_key_holder_num_of_keys_changed(new_num_of_keys: int) -> void:
 	_num_of_keys_label.text = str(new_num_of_keys)
+
+func _on_win_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		_attach_win_label()
+
+func _attach_win_label() -> void:
+	var win_label : Label = Label.new()
+	win_label.text = "You've won!"
+	get_tree().root.add_child(win_label)
+	win_label.position = get_viewport().get_camera_2d().global_position - win_label.size / 2
